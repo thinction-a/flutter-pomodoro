7fcea4b16b7610ef0e7e80ea652366166b923d2e
minseok Kim <jakuro94@gmail.com>
2023-01-17 16:53:01 +0900

    23.01.17 flutter practice with nomadCoders pomodoro

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Container(),
+      ),
+    );
+  }
+}
